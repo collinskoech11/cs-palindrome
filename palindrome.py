e0c4c9993a83a7ea3874c0deef89f3c902b175bd
Collins Koech <collinskoechck34@gmail.com>
2020-09-30 12:28:35 +0300

    update palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        a_pointer = 0
+        b_pointer = len(s) - 1
+
+        while a_pointer < b_pointer:
+
+            while a_pointer < b_pointer and not s[a_pointer].isalnum():
+                a_pointer += 1
+            while a_pointer < b_pointer and not s[b_pointer].isalnum():
+                b_pointer -= 1
+
+            if s[a_pointer].lower() != s[b_pointer].lower():
+                return false
+
+            a_pointer += 1
+            b_pointer -= 1
+
+        return true
+
