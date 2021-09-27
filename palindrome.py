c1ab99d6a36eaec3c2b9eb01c6d169cba2b34473
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2021-09-27 07:58:12 +0300

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -5,23 +5,23 @@ Note: For the purpose of this problem, we define empty string as valid palindrom
 """
 
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-
+    def isPalindrome(self, s:):
+        y = str(s)
         a_pointer = 0
-        b_pointer = len(s) - 1
+        b_pointer = len(y) - 1
 
         while a_pointer < b_pointer:
 
-            while a_pointer < b_pointer and not s[a_pointer].isalnum():
+            while a_pointer < b_pointer and not y[a_pointer].isalnum():
                 a_pointer += 1
-            while a_pointer < b_pointer and not s[b_pointer].isalnum():
+            while a_pointer < b_pointer and not y[b_pointer].isalnum():
                 b_pointer -= 1
 
-            if s[a_pointer].lower() != s[b_pointer].lower():
-                return false
+            if s[a_pointer].lower() != y[b_pointer].lower():
+                return False
 
             a_pointer += 1
             b_pointer -= 1
 
-        return true
+        return True
 
