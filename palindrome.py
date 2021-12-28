9192b8a8d9493c1d597ee3cad54e2e7119ccc982
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2021-12-28 16:25:09 +0300

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -3,7 +3,7 @@ Valid Palindrome:
 Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
 Note: For the purpose of this problem, we define empty string as valid palindrome.
 """
-#Binary SEarch Solution
+#Binary Search Solution
 class Solution:
     def isPalindrome(self, s:):
         y = str(s)
